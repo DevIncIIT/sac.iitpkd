a880e51d8e2ae73a3bf16cd64afcc43912ecc383
Abhi-Iyer10 <abhi.iyer10@gmail.com>
2024-11-14 22:46:09 +0530

    Create MainContent.svelte

---
+++ src/routes/DashBoard/LiveEvents/MainContent.svelte
@@ -0,0 +1,23 @@
+<script>
+  import EventCard from './EventCard.svelte';
+	let dummyDescription="Announcement/Event Description Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.";
+  let events = [
+    { club: "YACC", clublogo: "https://picsum.photos/100", title: "Web Dev BootCamp", description: dummyDescription, time: "12 March", date: "10:10 PM", venue: "Common Room", image: "https://picsum.photos/100/100" },
+    { club: "Shutterbug", clublogo: "https://picsum.photos/200", title: "Photography Workshop", description: dummyDescription, time: "12 March", date: "10:10 PM", venue: "Common Room", image: "https://picsum.photos/200/300" },
+    { club: "Voxel", clublogo: "https://picsum.photos/300", title: "Photoshop Workshop", description: dummyDescription, time: "12 March", date: "10:10 PM", venue: "Common Room", image: "https://picsum.photos/300/300" },
+  ];
+</script>
+
+<div class="main-content">
+  {#each events as event}
+    <EventCard {...event} />
+  {/each}
+</div>
+
+<style>
+  .main-content {
+    flex: 1;
+    padding: 20px;
+		color: black;
+  }
+</style>
