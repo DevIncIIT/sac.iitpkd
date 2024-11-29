cb872f4ec3d56c0fc1733a92921c41b9ccc4b143
GUNDAMARAJU HEMANTH KASHYAP <143811638+Hemanth-Kashyap-Gundamaraju@users.noreply.github.com>
2024-11-29 22:15:19 +0530

    Update Carousel.svelte

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,39 +1,55 @@
+<!-- Carousel1.svelte -->
 <script>
-	let imgurl;
-	let imgALT;
+  export let images = []; // An array of image objects with url and alt properties
 </script>
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src={imgurl} alt={imgALT} style="width:100%;">
+<div id="default-carousel" class="relative w-full" data-carousel="slide">
+  <!-- Carousel wrapper -->
+  <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+    {#each images as { url, alt }, index}
+      <!-- Item for each image -->
+      <div class={`hidden duration-700 ease-in-out ${index === 0 ? 'block' : ''}`} data-carousel-item>
+        <img src={url} alt={alt} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       </div>
-
-      <div class="item">
-        <img src=imgurl alt=imageALT style="width:100%;">
-      </div>
-    
-      <div class="item">
-        <img src=imgurl alt=imageALT style="width:100%;">
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
+    {/each}
   </div>
+
+  <!-- Slider indicators -->
+  <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+    {#each images as _, index}
+      <button
+        type="button"
+        class="w-3 h-3 rounded-full"
+        aria-current={index === 0 ? "true" : "false"}
+        aria-label={`Slide ${index + 1}`}
+        data-carousel-slide-to={index}
+      ></button>
+    {/each}
+  </div>
+
+  <!-- Slider controls -->
+  <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+      <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+      </svg>
+      <span class="sr-only">Previous</span>
+    </span>
+  </button>
+
+  <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+      <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+      </svg>
+      <span class="sr-only">Next</span>
+    </span>
+  </button>
 </div>
+
+<style>
+  .carousel-item img {
+    width: 100%;
+    height: auto;
+  }
+</style>
