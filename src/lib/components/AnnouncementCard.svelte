2c52b2b806fde6caba4cbb2391a48c95c6f8e28d
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-12-21 00:41:21 +0530

    Added announcement card

---
+++ src/lib/components/AnnouncementCard.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    type Announcement = {
+        title: string;
+        club_name: string;
+        image: string;
+        description: string;
+    };
+    let { title, club_name, image, description }: Announcement = $props();
+</script>
+
+<div class="main">
+    <h4>{club_name}</h4>
+    <h2>{title}</h2>
+    <div class="image-holder">
+        <img src={image} alt="{title} by {club_name}">
+    </div>
+    <p>{description}</p>
+</div>
+
+<style>
+    .main {
+        background-color: #f6a55f;
+        display: flex;
+        flex-direction: column;
+        border-radius: 1em;
+        padding: 1em;
+        border-radius: 1em;
+        border: black 2px solid;
+    }
+    .image-holder {
+        aspect-ratio: 1;
+        width: 100%;
+        object-fit: cover;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        border-radius: 1em;
+        border: black 2px solid;
+    }
+    img {
+        height: 100%;
+    }
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import AnnouncementCard from "$lib/components/AnnouncementCard.svelte";
+</script>
+
+<div>
+    <AnnouncementCard
+        title="WebDev Workshop"
+        club_name="YACC"
+        image="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
+        description="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    />
+</div>
+
+<style>
+    div {
+        width: 25em;
+        aspect-ratio: 1;
+    }
+</style>
