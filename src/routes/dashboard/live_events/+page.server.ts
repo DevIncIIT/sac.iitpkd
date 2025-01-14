826de2f3b65f1e985a59de2b6c52b3e350b2c36f
Iambeastofhell <visheshkratos@gmail.com>
2025-01-15 00:40:42 +0530

    Improvements

--- src/routes/dashboard/live_events/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { VIEW_EVENT_URL } from "$lib/server/urls";
-
-export const load = async ()=> {
-    const response = await fetch(VIEW_EVENT_URL);
-    const events = await response.json();
-    return {
-        events
-    };
-}
--- src/routes/dashboard/live_events/+page.svelte
+++ src/routes/dashboard/live_events/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import MainContent from './MainContent.svelte';
-  export let data ;
-  let events=data.events;
+  export let data ;  
+  let events = data.events;
+  console.log(data.events);
 </script>
 
 <div class="mt-3 ml-[2rem]">
