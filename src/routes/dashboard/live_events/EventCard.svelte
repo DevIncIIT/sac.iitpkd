b62fb99323ef991ae76e1fc7e097f8f1b4d58835
Iambeastofhell <visheshkratos@gmail.com>
2025-01-15 00:59:58 +0530

    Added images

--- src/routes/dashboard/live_events/EventCard.svelte
+++ src/routes/dashboard/live_events/EventCard.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { image_link } from "$lib";
 	export let club;
 	export let clublogo;
 	export let title;
@@ -6,16 +7,16 @@
 	export let venue;
 	export let image;
 	export let starts_at;
+	export let id;
 	const datestring = new Date(starts_at);
 	let time = datestring.toLocaleTimeString();
 	let date = datestring.toLocaleDateString();
-	
 </script>
 
 <div class="event-card">
 	<div class="content">
 		<div class="club-logo">
-			<img src={clublogo} alt={club+" Logo"} class="club-logo"/>
+			<img src={image_link(id, "Event")} alt={club+" Logo"} class="club-logo"/>
 		</div>
 		<div class="event-info">
 			<div class="club">
@@ -30,7 +31,7 @@
 	  </div>
 		<div class="event-details">
 			<div class="event-image">
-				<img src={image} alt={title} class="event-image"/>
+				<img src={image_link(id, "Event")} alt={club+" Logo"} />
 			</div>
 			<div class="event-data">
 				<p>Time - {time}</p>
