8605f3041aa842860f946453bf30b296bfecf4ab
hemant030406 <112201024@smail.iitpkd.ac.in>
2025-01-16 01:34:38 +0530

    cookie authorization header added in accept club application

--- src/routes/api/accept/+server.ts
+++ src/routes/api/accept/+server.ts
@@ -4,6 +4,10 @@ import type { RequestHandler } from "@sveltejs/kit";
 export const POST: RequestHandler = async ({ request }) => {
     return await fetch(ACCEPT_CLUB_APPL_URL, {
         method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${cookies.get('jwt')}`
+        },
         body: JSON.stringify(await request.json()),
     });
 };
