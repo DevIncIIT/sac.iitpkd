8a8e2aa59bace1fe91af76c5f46df1fbe999f429
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2025-01-15 21:09:59 +0530

    club page changed

--- src/routes/dashboard/clubs/[clubid]/+page.svelte
+++ src/routes/dashboard/clubs/[clubid]/+page.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
+    import { image_link } from "$lib";
     import type { PageData } from "./$types";
 
-    // let { data }: { data: PageData } = $props();
-    export let data;
+    let { data }: { data: PageData } = $props();
+    let club = $derived(data.data);
 </script>
 
 <main class="px-4 h-[50vh]">
-    <h1 class="text-4xl font-bold mb-4">{data.name.toUpperCase()}</h1>
+    <h1 class="text-4xl font-bold mb-4">{club.name.toUpperCase()}</h1>
     <div class="flex h-full py-2 gap-4">
         <div
             class="bg-cover bg-center bg-no-repeat rounded-lg"
-            style="background-image: url({data.image}); flex: 1;"
+            style="background-image: url({image_link(club.id, "Club")}); flex: 1;"
         ></div>
-        <p style="flex: 2;">{data.description}</p>
+        <p style="flex: 2;">{club.description}</p>
         <div style="flex: 1;" class="bg-[#f6a55f] p-4 rounded-lg">
-            <h3 class="text-xl font-bold">Heads of {data.name.toUpperCase()}</h3>
-            {#each data.heads as head}
+            <h3 class="text-xl font-bold">Heads of {club.name.toUpperCase()}</h3>
+            {#each club.heads as head}
                 <strong>{head.name}</strong>
 				<br>
                 <span>
